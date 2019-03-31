8f551cbfc2ac3c523e2c63c55e2538a2f5427690
Daiki Katayama <karuiad.venezia@gmail.com>
2019-03-31 15:39:24 +0900

    Initial Commit

---
+++ MVP_ConnpassEventSearchTests/MVP_ConnpassEventSearchTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVP_ConnpassEventSearchTests.swift
+//  MVP_ConnpassEventSearchTests
+//
+//  Created by Daiki Katayama on 2019/03/31.
+//  Copyright © 2019 Daiki Katayama. All rights reserved.
+//
+
+import XCTest
+@testable import MVP_ConnpassEventSearch
+
+class MVP_ConnpassEventSearchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
